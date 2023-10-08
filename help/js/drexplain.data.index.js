b73618acef87b7578dce75d3112ee8661b649708
David Das <David.das@maricopa.gov>
2023-10-08 13:14:29 -0700

    Added Help

---
+++ help/js/drexplain.data.index.js
@@ -0,0 +1,13 @@
+DR_EXPLAIN.namespace( 'DR_EXPLAIN.data_index' );
+DR_EXPLAIN.data_index = {
+
+	// index
+	DREX_NODE_KEYWORDS: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36],
+	DREX_NODE_KEYWORDS_START: [0,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35], //length:= drex.nodes_count,
+	DREX_NODE_KEYWORDS_END: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36], //length:= drex.nodes_count,
+
+	DREX_KEYWORD_NAMES: ["<NEW KEYWORD>","Overview","Quick Tour","Launching the Web App","List of Widgets and Tools","Navigation Tools","Graphical Scale Bar","Coordinate Widget","Search Tool","Identify a Parcel","Measure Tool","Draw Widget","Bookmark Widget","Coordinate Conversion Widget","Near Me Widget","Basemap Gallery Widget","Query Widget","Map Layers Widget","Legend Widget","Attribute Table Widget","Swipe Widget","Google Maps Widget","Pictometry Widget","Print Widget","About Widget","Summary","Contact Us","Home Button","Zoom In Tool","Zoom Out Tool","Pan Tool","Find My Location Tool","Using the Parcel Query","Using the Subdivision Query","Using the ZIP Code Query","Using the City Query","Using the Township, Range, Section Query"],
+	DREX_KEYWORD_CHILD_START: [1,27,27,27,27,27,32,32,32,32,32,32,32,32,32,32,32,37,37,37,37,37,37,37,37,37,37,37,37,37,37,37,37,37,37,37,37],
+	DREX_KEYWORD_CHILD_END: [27,27,27,27,27,32,32,32,32,32,32,32,32,32,32,32,37,37,37,37,37,37,37,37,37,37,37,37,37,37,37,37,37,37,37,37,37]
+	
+};
